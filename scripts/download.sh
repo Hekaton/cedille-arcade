cc0edfe90d84bb5a7201f0b3f3da3488c7bde6a5
Club Cedille <maxime.st-pierre.3@ens.etsmtl.ca>
2016-08-03 23:20:33 -0400

    Add a download script based on wget

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,3 @@
-#!bin/bash/
-
 # Script that download every rom in a list automaticaly and class them
 
 # Create the temporary directory
@@ -7,3 +5,40 @@ mkdir temp
 cd temp
 
 # Download each console seperatly with bios
+
+# Download GameBoyAdvance
+mkdir GameBoyAdvance
+cd GameBoyAdvance
+wget -i ../../GameBoyAdvance.txt
+unzip \*.zip
+cd  ..
+
+# Download NES
+mkdir Nintendo
+cd Nintendo
+wget -i ../../Nintendo.txt
+unzip \*.zip
+cd ..
+
+# Download N64
+mkdir Nintendo64
+cd Nintendo64
+wget -i ../../Nintendo64.txt
+unzip \*.zip
+cd ..
+
+# Download SegaGenesis
+mkdir SegaGenesis
+cd SegaGenesis
+wget -i ../../SegaGenesis.txt
+unzip \*.zip
+cd ..
+
+# Download SuperNintendo
+mkdir SuperNintendo
+cd SuperNintendo
+wget -i ../../SuperNintendo.txt
+unzip \*.zip
+cd ..
+
+echo "Gaming Time :D"
