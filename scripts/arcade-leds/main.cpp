1c2d3746741edcf456e700c28a18b2a75a56458f
Dmitri K <kolytchev.d@gmail.com>
2016-09-15 01:09:08 -0400

    [WIP] adding code that might work probably maybe?
    I hate working on X11 from windows. No, I didn't have a choice.
    Don't judge me.

--- scripts/arcade-leds/main.cpp
+++ scripts/arcade-leds/main.cpp
@@ -5,33 +5,58 @@
 
 using namespace std;
 
-static unsigned int PIXELS = 8*32;
+static unsigned int PIXELS_W = 64;
+static unsigned int PIXELS_H = 8;
+static unsigned int PIXELS = PIXELS_H * PIXELS_W;
 static unsigned int MAX_VALUE = 16;
 
 bool run = true;
 
 int main(int, char**)
 {
-    XColor c;
+
+    NeoPixel *n = new NeoPixel(PIXELS);
+
     Display *d = XOpenDisplay(0);
+    Screen *screen = DefaultScreen(d);
 
-	//TODO: get screen width and height
-    int x=720;  // Pixel x
-    int y=64;  // Pixel y
+    unsigned int side = Math.floor(Math.min(screen->height / PIXELS_H, screen->width / PIXELS_W));
+    unsigned int square = side * side;
+    unsigned int width = side * PIXELS_W;
+    unsigned int height = side * PIXELS_H;
 
-    XImage *image = XGetImage (d, RootWindow (d, DefaultScreen (d)), x, y, 1, 1, AllPlanes, XYPixmap);
-	c.pixel = XGetPixel (image, 0, 0);
+    double process = 1/square/65536*MAX_VALUE;
 
-	XFree (image);
+    while(run){
+        // get screen pixels
+        XImage *image = XGetImage (d, RootWindow (d, screen), 0, 0, width, height, AllPlanes, XYPixmap);
 
-	XQueryColor (d, DefaultColormap(d, DefaultScreen (d)), &c);
-    cout << c.red/256 << " " << c.green/256 << " " << c.blue/256 << "\n";
+        for(int ix = 0; ix < PIXELS_W; ix++){
+            for(int iy = 0; iy < PIXELS_H; iy++) {
 
-	NeoPixel *n = new NeoPixel(PIXELS);
+                double red, green, blue;
+                for(int i = 0; i<square; i++){
+                    Xcolor c;
+                    c.pixel = XGetPixel(image, ix * side + (i%side), iy * side + Math.floor(i/side));
+                    XQueryColor (d, DefaultColormap(d, screen), &c);
 
-	while (run) n->effectsDemo();
+                    // blend color channels
+                    // also X11 keeps values as 0-65535, dividing by 256 sets it back to 0-255
+                    red += c.red * process;
+                    green += c.green * process;
+                    blue += c.blue * process;
+                }
 
-	delete n;
-	delete image;
+                // TODO send neopixel matrix data;
+                int pixel = ix*PIXELS_H + iy; // LED matrix coords are {y, x} instead of {x, y}
+                n->setPixelColor(pixel, (char)Math.floor(red), (char)Math.floor(green), (char)Math.floor(blue));
+            }
+        }
+        n->show();
+        XFree (image);
+        cout << c.red/256 << " " << c.green/256 << " " << c.blue/256 << "\n";
+    }
+
+    delete n;
     return 0;
 }
